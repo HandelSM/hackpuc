5b18b342e5f11c9b7f8e4514736b475e7c614eaf
Handel Scholze Marques <hscholzem@gmail.com>
2015-12-12 00:41:38 -0200

    Coments generate scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212002300) do
+ActiveRecord::Schema.define(version: 20151212021417) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
