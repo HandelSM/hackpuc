cd4337f665dc0d67245aeb07b15a820a83526c11
Miguel <miguelszcorti@gmail.com>
2015-12-11 23:15:14 -0200

    Adding Laws

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212002300) do
+ActiveRecord::Schema.define(version: 20151212011445) do
+
+  create_table "laws", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
