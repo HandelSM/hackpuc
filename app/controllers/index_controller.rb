bae9c377e92ef4550d5b24c6fd201c34c9ad7e15
Handel Scholze Marques <hscholzem@gmail.com>
2015-12-11 23:18:02 -0200

    Creating index controller and seting it as root

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,11 @@
+class  IndexController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+
+  def teste
+		render text: "Bem-vindo ao Politicapp"
+	end
+
+
+end
