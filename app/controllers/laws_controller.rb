c524d22af9a5216f9204b98311aa28ccbca6c14a
Miguel <miguelszcorti@gmail.com>
2015-12-12 06:15:02 -0200

    Adding function comment

--- app/controllers/laws_controller.rb
+++ app/controllers/laws_controller.rb
@@ -83,6 +83,20 @@ class LawsController < ApplicationController
     redirect_to @lawNext
   end
 
+  def comment
+    @coment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_law
