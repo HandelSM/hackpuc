f2c7f64a8893c3323081dd7a868f3d8c5e171c0d
Lucas Pavanelli <lucasapava@gmail.com>
2015-12-13 03:05:32 -0200

    Changing css in signin and signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
+  get    'estatistica' => 'laws#index'
   get    'semLeis' => 'laws#noLaws'
   get    'lawNext' => 'laws#nextLaw'
   get    'signup'  => 'users#new'
